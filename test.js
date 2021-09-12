274c87cd75f79609d7a2f3889c2858fdcce7c7cb
totoguri <finalyo4@naver.com>
2021-09-12 22:24:23 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+// function add(x, y) {
+//     return x + y;
+// }
+
+// console.dir(add);
+// console.log(add(2, 5));
+
+// var add = function add(x, y) {
+//     return x + y;
+// };
+
+// console.log(add(2, 5));
+
+// p.160
+
+var add = function add(x, y) {
+    return x + y;
+};
+
+console.log(add(2, 5));
+
+// p.163
